d4ea1b39ec53f98cdd5b373212381ac545aeb200
Abdelhafid HADJADJ <98639038+abdelhafidHadjadj@users.noreply.github.com>
2023-09-19 19:16:37 +0100

    v0.0

---
+++ src/Component/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return(
+        <footer>
+            <div>Bloc1</div>
+            <div>Bloc2</div>
+            <div>Bloc3</div>
+
+        </footer>
+        )
+}
+export default Footer
